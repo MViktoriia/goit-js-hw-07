71693ba141e24c194a875950e25aa970155d430d
Viktoriia Mykhalova <mihaleva.victoria@gmail.com>
2022-01-22 22:19:24 +0200

    Add Task One Begining

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,25 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryRef = document.querySelector('.gallery');
+
+
+const galleryItemsRef = galleryItems.map((item) => 
+`<div class="gallery__item"><a class="gallery__link" href="${item.original}"><img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}"/></a></div>`).join("");
+console.log(galleryItemsRef);
+
+galleryRef.insertAdjacentHTML("afterbegin", galleryItemsRef);
+
+
+galleryRef.addEventListener("click", onGalleryImageClickHandler);
+
+function onGalleryImageClickHandler(event) {
+    event.preventDefault();
+    console.log(event.target.nodeName);
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+    
+};
