8ba728b4566be398110d56acf7b103ea144ff44e
Viktoriia Mykhalova <mihaleva.victoria@gmail.com>
2022-01-23 22:31:38 +0200

    add task 1 final and markup for task 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -9,18 +9,24 @@ const galleryRef = document.querySelector('.gallery');
 
 const galleryItemsRef = galleryItems.map((item) => 
 `<div class="gallery__item"><a class="gallery__link" href="${item.original}"><img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}"/></a></div>`).join("");
-console.log(galleryItemsRef);
+
 
 galleryRef.insertAdjacentHTML("afterbegin", galleryItemsRef);
 
 
-galleryRef.addEventListener("click", onGalleryImageClickHandler);
+galleryRef.addEventListener("click", onGalleryImageClick);
 
-function onGalleryImageClickHandler(event) {
+function onGalleryImageClick(event) {
     event.preventDefault();
-    console.log(event.target.nodeName);
+    
     if (event.target.nodeName !== "IMG") {
         return;
     }
+    console.log(event.target.dataset.source);
+
+    basicLightbox.create(`
+		<img src="${event.target.dataset.source}">
+	`).show()
+
     
 };
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,12 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+
+
+const galleryItemsRef = galleryItems.map((item) => 
+`<a class="gallery__link" href="${item.original}"><img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}"/></a>`).join("");
+
+
+galleryRef.insertAdjacentHTML("afterbegin", galleryItemsRef);
